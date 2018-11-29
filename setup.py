246851048e59cbd5727f5095deff550cb1197a7e
Jamie Hardt <jamiehardt@me.com>
2018-11-29 14:23:44 -0800

    Added license and updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pycmx',
       version='0.1',
-      description='CMX3600 Edit Decision List Parser',
-      url='https://github.com/iluvcapra/pycmx/',
       author='Jamie Hardt',
       author_email='jamiehardt@me.com',
-      license='MIT',
-      packages=['pycmx'],
-      zip_safe=False)
+      description='CMX3600 Edit Decision List Parser',
+      long_description_content_type="text/markdown",
+      long_description=long_description,
+      url='https://github.com/iluvcapra/pycmx',
+      packages=['pycmx'])
