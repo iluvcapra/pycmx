e5f632d8a4c127cc870917c0d97fc87d0232c4ab
Jamie Hardt <jamiehardt@me.com>
2018-12-01 13:08:16 -0800

    Updated setup, mnudge to version 0.5

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pycmx',
-      version='0.4',
+      version='0.5',
       author='Jamie Hardt',
       author_email='jamiehardt@me.com',
       description='CMX 3600 Edit Decision List Parser',
       long_description_content_type="text/markdown",
       long_description=long_description,
       url='https://github.com/iluvcapra/pycmx',
-      classifiers=['Development Status :: 3 - Alpha',
+      classifiers=['Development Status :: 4 - Beta',
           'License :: OSI Approved :: MIT License',
           'Topic :: Multimedia',
           'Topic :: Multimedia :: Video',
