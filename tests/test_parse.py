30ee3e0be5e5a4f932eb9f27458a41b7f9d6cbe3
Jamie Hardt <jamiehardt@me.com>
2018-12-24 17:32:23 -0800

    Update test_parse.py

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -4,14 +4,15 @@ import pycmx
 
 class TestParse(TestCase):
 
-    def test_edls(self):
+    def test_event_counts(self):
         files = ["INS4_R1_010417.edl" ,
+                "INS4_R1_DX_092117.edl",
                   "STP R1 v082517.edl",
                   "ToD_R4_LOCK3.1_030618_Video.edl",
                   "TEST.edl"
                 ]
         
-        counts = [ 287, 250 , 376, 120  ]
+        counts = [ 287, 466,  250 , 376, 120  ]
 
 
         for fn, count in zip(files, counts):
