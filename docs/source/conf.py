cbec18607a9555d91ff810c69cf4055de7e30d9b
Jamie Hardt <jamiehardt@me.com>
2018-12-26 21:38:10 -0800

    Doc buildinfo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Jamie Hardt'
 author = 'Jamie Hardt'
 
 # The short X.Y version
-version = ''
+version = '0.7'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------
@@ -75,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
