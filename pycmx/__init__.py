16e8754a7b4d359ff67717833ce0e2e9a2ac8b31
Jamie Hardt <jamiehardt@me.com>
2018-12-28 23:08:22 -0800

    Merge branch master into release

--- pycmx/__init__.py
+++ pycmx/__init__.py
@@ -8,7 +8,7 @@ copy and reuse this software, refer to the LICENSE file included with the
 distribution.
 """
 
-__version__ = '0.7'
+__version__ = '0.8'
 __author__  = 'Jamie Hardt'
 
 from .parse_cmx_events import parse_cmx3600, Transition, Event, Edit
