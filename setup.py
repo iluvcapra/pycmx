8b49a788ae15f77c3fa1e88a7283bb520713909e
Jamie Hardt <jamiehardt@me.com>
2019-08-17 13:01:51 -0700

    Add version 3.7 support checks

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(name='pycmx',
           'Topic :: Text Processing',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           ],
       packages=['pycmx'])
