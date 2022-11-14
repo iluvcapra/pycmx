6ba77b3568a7e66a6bbd83bc3d558cd28aedfba1
Jamie Hardt <jamiehardt@me.com>
2022-11-13 17:32:30 -0800

    Adding newer python versions
    to setup.rb and test grid

--- setup.py
+++ setup.py
@@ -16,10 +16,11 @@ setup(name='pycmx',
           'Topic :: Multimedia',
           'Topic :: Multimedia :: Video',
           'Topic :: Text Processing',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8'
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'
           ],
       packages=['pycmx'])
