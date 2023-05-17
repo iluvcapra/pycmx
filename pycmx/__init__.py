173493a6104901f46cb211a9104e05fd6deee1a2
Jamie Hardt <jamiehardt@me.com>
2023-05-17 16:12:42 -0700

    Updated project to pyproject-style package

--- pycmx/__init__.py
+++ pycmx/__init__.py
@@ -3,13 +3,12 @@
 pycmx is a module for parsing CMX 3600-style EDLs. For more information and
 examples see README.md
 
-This module (c) 2018 Jamie Hardt. For more information on your rights to
+This module (c) 2022 Jamie Hardt. For more information on your rights to
 copy and reuse this software, refer to the LICENSE file included with the
 distribution.
 """
 
-__version__ = '1.1.1'
-__author__  = 'Jamie Hardt'
+__version__ = '1.2.0'
 
 from .parse_cmx_events import parse_cmx3600
 from .transition import Transition
