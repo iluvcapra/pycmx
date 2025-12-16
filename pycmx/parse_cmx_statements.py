2ee94ca358eda9bd5dbcda6830ad9e6985e31a73
Jamie Hardt <jamie@squad51.us>
2025-12-16 09:44:36 -0800

    Moved statement structs into a new file

--- pycmx/parse_cmx_statements.py
+++ pycmx/parse_cmx_statements.py
@@ -5,25 +5,9 @@ import re
 from collections import namedtuple
 from typing import TextIO, List
 
-
+from .statements import *
 from .util import collimate
 
-StmtTitle = namedtuple("Title", ["title", "line_number"])
-StmtFCM = namedtuple("FCM", ["drop", "line_number"])
-StmtEvent = namedtuple("Event", ["event", "source", "channels", "trans",
-                                 "trans_op", "source_in", "source_out",
-                                 "record_in", "record_out", "format",
-                                 "line_number"])
-StmtAudioExt = namedtuple("AudioExt", ["audio3", "audio4", "line_number"])
-StmtClipName = namedtuple("ClipName", ["name", "affect", "line_number"])
-StmtSourceFile = namedtuple("SourceFile", ["filename", "line_number"])
-StmtRemark = namedtuple("Remark", ["text", "line_number"])
-StmtEffectsName = namedtuple("EffectsName", ["name", "line_number"])
-StmtSourceUMID = namedtuple("Source", ["name", "umid", "line_number"])
-StmtSplitEdit = namedtuple("SplitEdit", ["video", "magnitude", "line_number"])
-StmtMotionMemory = namedtuple(
-    "MotionMemory", ["source", "fps"])  # FIXME needs more fields
-StmtUnrecognized = namedtuple("Unrecognized", ["content", "line_number"])
 
 
 def parse_cmx3600_statements(file: TextIO) -> List[object]:
---
+++ pycmx/statements.py
@@ -0,0 +1,18 @@
+from collections import namedtuple 
+
+StmtTitle = namedtuple("Title", ["title", "line_number"])
+StmtFCM = namedtuple("FCM", ["drop", "line_number"])
+StmtEvent = namedtuple("Event", ["event", "source", "channels", "trans",
+                                 "trans_op", "source_in", "source_out",
+                                 "record_in", "record_out", "format",
+                                 "line_number"])
+StmtAudioExt = namedtuple("AudioExt", ["audio3", "audio4", "line_number"])
+StmtClipName = namedtuple("ClipName", ["name", "affect", "line_number"])
+StmtSourceFile = namedtuple("SourceFile", ["filename", "line_number"])
+StmtRemark = namedtuple("Remark", ["text", "line_number"])
+StmtEffectsName = namedtuple("EffectsName", ["name", "line_number"])
+StmtSourceUMID = namedtuple("Source", ["name", "umid", "line_number"])
+StmtSplitEdit = namedtuple("SplitEdit", ["video", "magnitude", "line_number"])
+StmtMotionMemory = namedtuple(
+    "MotionMemory", ["source", "fps"])  # FIXME needs more fields
+StmtUnrecognized = namedtuple("Unrecognized", ["content", "line_number"])
