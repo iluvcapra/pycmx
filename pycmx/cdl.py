f56a2aef4f418c0a8d178a1df84c71b6b8fceac0
Jamie Hardt <jamie@squad51.us>
2025-12-16 16:56:55 -0800

    tweaking generics for <3.10 support

--- pycmx/cdl.py
+++ pycmx/cdl.py
@@ -1,18 +1,19 @@
 # pycmx
 # (c) 2025 Jamie Hardt
 
+from dataclasses import dataclass
 from typing import Generic, NamedTuple, TypeVar
 
 T = TypeVar('T')
 
-
-class Rgb(NamedTuple, Generic[T]):
+@dataclass
+class Rgb(Generic[T]):
     red: T
     green: T
     blue: T
 
-
-class AscSopComponents(NamedTuple, Generic[T]):
+@dataclass
+class AscSopComponents(Generic[T]):
     """
     Fields in an ASC SOP (Slope-Offset-Power) color transfer function
     statement
